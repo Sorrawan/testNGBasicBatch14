aefe2d3b6a01cc79bd1fb3ce8264cf6c3055ec74
Sorrawan <sorrawan12@gmail.com>
2022-12-01 22:31:38 -0500

    review01

---
+++ src/test/java/review1/AnnotationExample.java
@@ -0,0 +1,15 @@
+package review1;
+
+import org.testng.annotations.Test;
+
+public class AnnotationExample {
+    @Test
+    public void myFirstTestCase(){
+        System.out.println("This is my first test case");
+    }
+
+    @Test
+    public void anotherTestCase(){
+        System.out.println("This is my another test case");
+    }
+}
